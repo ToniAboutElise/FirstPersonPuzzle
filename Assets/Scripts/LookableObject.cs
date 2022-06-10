6e2c79b0008ab3310c334875daa2386ed826226c
DESKTOP-6DK5DIA\tonid <tonidominguezreig@gmail.com>
2022-06-10 15:03:10 +0200

    Creates and adds Textures folder to LFS, createts copper texture and material and makes lookable obejct to rotate with move instead of camera

--- Assets/Scripts/LookableObject.cs
+++ Assets/Scripts/LookableObject.cs
@@ -36,8 +36,8 @@ public class LookableObject : InteractableObject
     public override void WhileInteracting()
     {
         base.WhileInteracting();
-        _clampedYInput = Mathf.Clamp(Player.instance.GetPlayerInputActions().Player.Camera.ReadValue<Vector2>().y, -1, 1);
-        _clampedXInput = Mathf.Clamp(Player.instance.GetPlayerInputActions().Player.Camera.ReadValue<Vector2>().x, -1, 1);
+        _clampedYInput = Mathf.Clamp(Player.instance.GetPlayerInputActions().Player.Move.ReadValue<Vector2>().y, -1, 1);
+        _clampedXInput = Mathf.Clamp(Player.instance.GetPlayerInputActions().Player.Move.ReadValue<Vector2>().x, -1, 1);
         _interactableObject.transform.Rotate(new Vector3(_clampedYInput * 2, _clampedXInput * 2, 0), Space.Self);
     }
 }
