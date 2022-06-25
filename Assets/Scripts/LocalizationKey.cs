d8c83089ee72c1a640e5c50c824205c4a17f0ddf
DESKTOP-6DK5DIA\tonid <tonidominguezreig@gmail.com>
2022-06-25 16:42:01 +0200

    Implements break lines with $$

--- Assets/Scripts/LocalizationKey.cs
+++ Assets/Scripts/LocalizationKey.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LocalizationKey : MonoBehaviour
 {
     [SerializeField] private string _key;
-    private Text _keyText;
+    private TMP_Text _keyText;
 
     public string GetKey() { return _key; }
-    public void SetKeyText(string keyText) { _keyText.text = keyText; }
+    public void SetKeyText(string keyText) { _keyText.text = keyText; _keyText.text = _keyText.text.Replace("$$", "\n"); }
 
     private void Awake()
     {
-        _keyText = GetComponent<Text>();
+        _keyText = GetComponent<TMP_Text>();
     }
 }
